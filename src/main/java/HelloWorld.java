359ad03a93832dce5cfc153ad9ece3292afe4e9c
Katya Zeland <burlakkatya8@gmail.com>
2023-12-05 13:46:37 +0200

    feat: add text long text

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,14 @@ public class HelloWorld {
     public static void main(String[] args) {
      int myInt;
      myInt = 557;
-     System.out.println(myInt);
-     System.out.println(myInt);
+     short myShort =3255;
+     long mylong = 234456008;
+     double myDouble = 345.98;
+     float myFloat = 5643.9f;
+     char c = 'a';
+     boolean b = true;
+     byte myByte = 100;  //-128 -127
+     System.out.println(myByte);
+
     }
 }
