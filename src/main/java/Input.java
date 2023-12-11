9fb6cdfa0b3590867a2caffaa80a022456f3983e
Katya Zeland <burlakkatya8@gmail.com>
2023-12-11 22:40:23 +0200

    feat: Scanner Do while text

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int value= scanner.nextInt();
-        while (value!=5) {
+        int value;
+        do {
             System.out.println("¬веди 5");
             value = scanner.nextInt();
-        }
+        } while (value!=5);
         System.out.println("вы ввели 5");
         }
     }
