4daab8f2bb03bf4da0be2c4205b2abaec41f8d7e
Katya Zeland <burlakkatya8@gmail.com>
2023-12-11 23:07:30 +0200

    feat:  text Continue

--- src/main/java/BreakContinue.java
+++ src/main/java/BreakContinue.java
@@ -1,13 +1,10 @@
 public class BreakContinue {
     public static void main(String[] args) {
-        int i = 0;
-        while (true){
-            if (i == 15){
-                break;
+        for (int i =0; i <=15; i++){
+            if (i%2==0){
+                continue;
             }
-            System.out.println(i);
-            i++;
+            System.out.println("ето нечотное число " +i);
         }
-        System.out.println("вы вышли из цикла.");
         }
     }
