fe12086c2c734133fad02a39e1d6c14bfd3a7523
Katya Zeland <burlakkatya8@gmail.com>
2023-12-11 22:30:52 +0200

    feat: Scanner int

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("¬ведить что-нибудь");
-        String string = s.nextLine();
-        System.out.println("вы ввели " +string);
+        System.out.println("¬ведить какое-нибудь число");
+        int x  = s.nextLine();
+        System.out.println("вы ввели " + x);
         }
     }
