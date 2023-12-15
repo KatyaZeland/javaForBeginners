469e7661ae56b59ebf2fe4bcf49c42ddf92efe04
Katya Zeland <burlakkatya8@gmail.com>
2023-12-15 16:08:45 +0200

    feat:  masiv matrice1

---
+++ src/main/java/Multidimensional_arrays.java
@@ -0,0 +1,8 @@
+public class Multidimensional_arrays {
+    public static void main(String[] args) {
+int[] numbers = {1, 2, 3};
+int[][] matrice ={{1,2,3}, {4, 5, 6},{7, 8, 9}};
+System.out.println(matrice[2][2]);
+System.out.println(matrice[1][0]);
+    }
+}
