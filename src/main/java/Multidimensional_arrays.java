31000ae4cf69d86977dd9b5a70233ecdcdb0e86a
Katya Zeland <burlakkatya8@gmail.com>
2023-12-15 16:16:33 +0200

    feat:  masiv matrice3

--- src/main/java/Multidimensional_arrays.java
+++ src/main/java/Multidimensional_arrays.java
@@ -1,9 +1,13 @@
 public class Multidimensional_arrays {
     public static void main(String[] args) {
-int[] numbers = {1, 2, 3};
+int[] number = {1, 2, 3};
 int[][] matrice ={{1,2,3, 4, 5}, {4, 5, 6},{7, 8, 9}};
 System.out.println(matrice[2][2]);
 System.out.println(matrice[1][0]);
 System.out.println(matrice[0][4]);
+int[] number1 = new int[5];
+String[][] strings = new String[2][3];
+strings[0][1] ="привет";
+System.out.println(strings[0][1]);
     }
 }
