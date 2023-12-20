142366df338a8c72fd5bc0fc164385c4b58ead6c
Katya Zeland <burlakkatya8@gmail.com>
2023-12-20 15:49:48 +0200

    feat: ClassesAndObjects1new

--- src/main/java/ClassesAndObjects.java
+++ src/main/java/ClassesAndObjects.java
@@ -2,20 +2,26 @@ public class ClassesAndObjects {
     public  static void main(String[] args) {
       Person person1 = new Person();
       person1.name = "Рома";
-      person1.age = 50;
+      person1.age = 40;
       person1.speak();
         person1.sayHello();
       Person person2 = new Person();
       person2.name = "Вова";
       person2.age = 20;
-      person2.speak();
-      person2.sayHello();
+     int year1 = person1.calculateYearsToRetirement();
+     int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии: " + year1+ "лет" );
+        System.out.println("Второму человеку до пенсии: " + year2+ "лет" );
     }
 }
 class Person{
     String name;
     int age;
- void  speak(){
+ int calculateYearsToRetirement(){
+     int years =65-age;
+     return years;
+ }
+    void  speak(){
      for (int i=0; i<3;i++) {
          System.out.println("Меня зовут " + name + "," + "мне " + age + " лет");
      }
