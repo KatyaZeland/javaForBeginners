8974c7d7c12411104064b5b3b9077b4c3fc23ef4
Katya Zeland <burlakkatya8@gmail.com>
2023-12-21 09:42:23 +0200

    feat: ClassesAndObjects1setNameAndAge1

--- src/main/java/ClassesAndObjects.java
+++ src/main/java/ClassesAndObjects.java
@@ -1,32 +1,55 @@
+import javax.swing.*;
+
 public class ClassesAndObjects {
-    public  static void main(String[] args) {
-      Person person1 = new Person();
-      person1.setNameAndAge("Рома",20);
-      String s1 = "Вова";
-      Person person2 = new Person();
-      person2.setNameAndAge(s1,30);
-     person1.speak();
-     person2.speak();
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setName("Маша");
+        person1.setAge(-6);
+        System.out.println("Выводим значение в main методи:" + person1.getName());
+        System.out.println("Выводим значение в main методи:" + person1.getAge());
     }
 }
-class Person{
-    String name;
-    int age;
- void  setNameAndAge(String username, int userage){
-     name = username;
-     age = userage;
- }
-    int calculateYearsToRetirement(){
-     int years =65-age;
-     return years;
- }
-    void  speak(){
-     for (int i=0; i<3;i++) {
-         System.out.println("Меня зовут " + name + "," + "мне " + age + " лет");
-     }
- }
-     void sayHello(){
-         System.out.println("Привет!");
-     }
- }
 
+class Person {
+    private String name;
+    private int age;
+
+    public void setName(String userName) {
+        if (userName.isEmpty()) {
+            System.out.println("Ты ввел пустое имя");
+        } else {
+            name = userName;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int userAge) {
+        if (userAge < 0) {
+            System.out.println("Возраст должен быть положительным");
+        } else {
+            age = userAge;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int calculateYearsToRetirement() {
+        int years = 65 - age;
+        return years;
+    }
+
+    void speak() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Меня зовут " + name + "," + "мне " + age + " лет");
+        }
+    }
+
+    void sayHello() {
+        System.out.println("Привет!");
+    }
+}
